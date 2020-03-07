168f54ae771eb071660ad64c34152720d90079f7
newbiesitl <yuboz@fb.com>
2020-03-06 18:02:09 -0800

    add screenshot

--- visual_listener.py
+++ visual_listener.py
@@ -1,11 +1,20 @@
 import keyboard
 import time
+import pyautogui
+import numpy as np
+
 max_queue_size = 100
 queue = []
 keyboard.start_recording(recorded_events_queue=queue)
 
 while True:
     queue = keyboard.stop_recording()
+    if len(queue) > 0:
+        last_event = queue[-1]
+        myScreenshot = pyautogui.screenshot()
+        pix = np.array(myScreenshot)
+        # pix is 4 channels they are r g b a
+        # alpha defines the transparency
     while len(queue) > 0:
         print(queue[0])
         queue.pop(0)
