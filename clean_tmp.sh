ee808e71c95d8c2d3d33f38404cf6c168991ce83
newbiesitl <yuboz@fb.com>
2020-03-06 21:51:40 -0800

    working

---
+++ clean_tmp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find ./ -name '*screenshot*' -delete
--- visual_listener.py
+++ visual_listener.py
@@ -1,44 +1,41 @@
 import keyboard
 import time
 import pyautogui
-
+import asyncio
 import numpy as np
-def record(record_current=True, screen_shot=True):
+
+async def async_screen_shot():
+    print('screen shot', time.time(), )
+    myScreenshot = pyautogui.screenshot().resize((1280, 800))
+    np.array(myScreenshot)
+
+async def async_record_event(fps):
     queue = []
     keyboard.start_recording(recorded_events_queue=queue)
-    pix = None
+    await asyncio.sleep(1/fps)
+    queue = keyboard.stop_recording()
+    for event in queue:
+        print(event.name, event.event_type, event.time, )
+
+
+def record(fps=50):
     while True:
-        time.sleep(1/30)
-        queue = keyboard.stop_recording()
-        if record_current and screen_shot:
-            myScreenshot = pyautogui.screenshot().resize((1280,800))
-            # myScreenshot.save('temp.png')
-            pix = np.array(myScreenshot)
-        if len(queue) > 0:
-            first_event = queue[0]
-            last_event = queue[-1]
-            # screen shot the last key event
-            # pix is 4 channels they are r g b a
-            # alpha defines the transparency
-            print('total events %d, first event %s last event %s' % (len(queue), queue[0], queue[-1]))
-            yield (pix, (first_event, last_event))
-
-        while len(queue) > 0:
-            queue.pop(0)
-
+        # asyncio.run(async_record_event(fps))
+        queue = []
         keyboard.start_recording(recorded_events_queue=queue)
-        if not record_current and screen_shot:  # record the next 1/30 sec key activity
-            myScreenshot = pyautogui.screenshot().resize((1280, 800))
-            # myScreenshot.save('temp.png')
-            pix = np.array(myScreenshot)
+        asyncio.run(async_screen_shot())
+        # time.sleep(1/fps)
+        queue = keyboard.stop_recording()
+        for event in queue:
+            print(event.name, event.event_type, event.time, )
+        # await asyncio.sleep(1/fps)
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
-    for event_frame in record(record_current=False, screen_shot=True):
-        pix, event = event_frame
-        if pix is not None:
-            print(pix.shape, event)
-        else:
-            print(event)
+    record(fps=20)
