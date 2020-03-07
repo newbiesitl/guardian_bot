de2d504b19e67e5ed351be17a431828a0e9320cc
newbiesitl <yuboz@fb.com>
2020-03-06 19:16:01 -0800

    add listener

--- visual_listener.py
+++ visual_listener.py
@@ -1,24 +1,44 @@
 import keyboard
 import time
 import pyautogui
+
 import numpy as np
-
-max_queue_size = 100
-queue = []
-keyboard.start_recording(recorded_events_queue=queue)
-
-while True:
-    queue = keyboard.stop_recording()
-    if len(queue) > 0:
-        last_event = queue[-1]
-        myScreenshot = pyautogui.screenshot()
-        pix = np.array(myScreenshot)
-        # pix is 4 channels they are r g b a
-        # alpha defines the transparency
-    while len(queue) > 0:
-        print(queue[0])
-        queue.pop(0)
+def record(record_current=True, screen_shot=True):
+    queue = []
     keyboard.start_recording(recorded_events_queue=queue)
-    time.sleep(1/30)
+    pix = None
+    while True:
+        time.sleep(1/30)
+        queue = keyboard.stop_recording()
+        if record_current and screen_shot:
+            myScreenshot = pyautogui.screenshot().resize((1280,800))
+            # myScreenshot.save('temp.png')
+            pix = np.array(myScreenshot)
+        if len(queue) > 0:
+            first_event = queue[0]
+            last_event = queue[-1]
+            # screen shot the last key event
+            # pix is 4 channels they are r g b a
+            # alpha defines the transparency
+            print('total events %d, first event %s last event %s' % (len(queue), queue[0], queue[-1]))
+            yield (pix, (first_event, last_event))
+
+        while len(queue) > 0:
+            queue.pop(0)
+
+        keyboard.start_recording(recorded_events_queue=queue)
+        if not record_current and screen_shot:  # record the next 1/30 sec key activity
+            myScreenshot = pyautogui.screenshot().resize((1280, 800))
+            # myScreenshot.save('temp.png')
+            pix = np.array(myScreenshot)
 
 
+
+
+if __name__ == "__main__":
+    for event_frame in record(record_current=False, screen_shot=True):
+        pix, event = event_frame
+        if pix is not None:
+            print(pix.shape, event)
+        else:
+            print(event)
