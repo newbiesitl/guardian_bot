fefa3844fd0a176c7c8e47a368f6b57e8b056695
charleschou <yubo@cse.yorku.ca>
2020-05-04 10:02:51 -0700

    upload reload timer

--- prevent_afk.py
+++ prevent_afk.py
@@ -4,29 +4,24 @@ import random
 print(KEYBOARD_KEYS)
 keyDown('right')
 keyUp('right')
+def reload():
+    press('enter')
+    for char in '/reload':
+        press(char)
+    press('enter')
+reload_counter = 20
+
 while True:
     try:
-        time.sleep(random.randint(10,60))
-        keyDown('right')
-        keyUp('right')
+        time.sleep(10)
+        if reload_counter == 0:
+            reload_counter = 2
+            reload()
+        reload_counter -= 1
+        # keyDown('right')
+        # keyUp('right')
     except Exception as e:
         print(e)
         keyUp('right')
         time.sleep(1)
         continue
-# keyUp('space')
-# time.sleep(1)
-# keyDown('backspace')
-# time.sleep(1)
-# keyUp('backspace')
-
-
-
-# keyDown('command')
-# time.sleep(1)
-#
-# keyDown('tab')
-# time.sleep(0.1)
-# keyUp('tab')
-# keyUp('command')
-
