72209dd27ad54bffa40ac7cd182a5cbb8a786afa
charleschou <yubo@cse.yorku.ca>
2020-05-04 11:41:01 -0700

    upload reload timer

--- prevent_afk.py
+++ prevent_afk.py
@@ -2,24 +2,34 @@ from pyautogui import keyDown, keyUp, press, KEYBOARD_KEYS
 import time
 import random
 print(KEYBOARD_KEYS)
-keyDown('right')
-keyUp('right')
+
 def reload():
     press('enter')
     for char in '/reload':
         press(char)
     press('enter')
+
+
+def send_message(char_id, msg):
+    press('enter')
+    msg = '/w %s %s' % (char_id, msg)
+    for char in msg:
+        press(char)
+    press('enter')
 reload_counter = 20
-counter_reset = 20
+counter_reset = 30
+time.sleep(5)
+send_message('Warag', 'test started')
 while True:
     try:
-        time.sleep(10)
+        time.sleep(30)
         if reload_counter == 0:
             reload_counter = counter_reset
             reload()
         reload_counter -= 1
-        # keyDown('right')
-        # keyUp('right')
+        keyDown('right')
+        keyUp('right')
+
     except Exception as e:
         print(e)
         keyUp('right')
