aff33d8585f903fa98013b89f549a412f0704561
charles <yubo@cse.yorku.ca>
2020-05-10 13:22:26 -0700

    working on rules and action functions

--- restful/main.py
+++ restful/main.py
@@ -7,7 +7,8 @@ import os, sys
 cur_path = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.join(cur_path, '..')
 sys.path.append(root_dir)
-from restful.bots.pally import pally_event_loop
+
+from pally import pally_event_loop
 
 time.sleep(5)
 class Item(BaseModel):
