ea9630ec2e24fb23026ba440f327cd43db341d6b
charles <yubo@cse.yorku.ca>
2020-05-10 13:27:30 -0700

    working on rules and action functions

--- restful/main.py
+++ restful/main.py
@@ -8,8 +8,6 @@ cur_path = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.join(cur_path, '..')
 sys.path.append(root_dir)
 
-from restful.pally import pally_event_loop
-
 time.sleep(5)
 class Item(BaseModel):
     name: str
@@ -58,3 +56,47 @@ def perform_action(item: Item):
     j_load = json.loads(json_body)
     pally_event_loop(j_load)
     return j_load
+
+
+def pally_event_loop(state):
+    global previous_state_in_combat
+    check_target()
+    health = int(state['targetHealth'])
+    max_health = int(state['targetHealthMax'])
+    if max_health == 0:
+        follow_target()
+    print('target combat', state['targetInCombat'])
+    print('targetHealth  percentage %d/%d' % (state['targetHealth'], state['targetHealthMax']))
+    if float(health/max_health) < 0.85:
+        heal_target()
+    elif float(state['health'])/100 < 0.85:
+        heal_self()
+    elif not state['targetInCombat'] and previous_state_in_combat:
+        mount()
+    else:
+        press('7') # cleanse self
+        follow_target()
+        cleanse()
+    previous_state_in_combat = state['targetInCombat']
+
+
+def check_target():
+    press('7')
+
+
+def follow_target():
+    press('3')
+
+
+def heal_target():
+    press('right')
+    press('g')
+
+
+def heal_self():
+    press('left')
+    press('2')
+
+
+def cleanse():
+    press('y')
